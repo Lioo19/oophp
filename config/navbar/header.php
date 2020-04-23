2a8426c608f5ff2b697f2325859fb8e1b369fe1e
Linnea <linnea@Samis-MBP.lan>
2020-04-23 12:03:11 +0200

    Last commit for kmom03

--- config/navbar/header.php
+++ config/navbar/header.php
@@ -30,6 +30,16 @@ return [
                         "url" => "redovisning/kmom02",
                         "title" => "Redovisning för kmom02.",
                     ],
+                    [
+                        "text" => "Kmom03",
+                        "url" => "redovisning/kmom03",
+                        "title" => "Redovisning för kmom03.",
+                    ],
+                    [
+                        "text" => "Kmom04",
+                        "url" => "redovisning/kmom04",
+                        "title" => "Redovisning för kmom04.",
+                    ],
                 ],
             ],
         ],
